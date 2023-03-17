75decd2033eca727952f21abaaa6864398360fab
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-03-17 22:19:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance<400){
+    return "This one is on me!"
+  }
+  else if (distance>2000 && distance<=2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (distance>2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// console.log (scuberGreetingForFeet(2600))
+
+function ternaryCheckCity(city){
+
+  return city === 'NYC'? 'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let answ;
+
+  switch (tip){
+    case 'generous': 
+      answ = 'Thank you so much.';
+      break;
+    case 'not as generous': 
+      answ= 'Thank you';
+      break;
+    default: 
+      answ='Bye.';
+      break;
+
+  }
+return answ
 }
+
+console.log (switchOnCharmFromTip('not as generous'))
